39a00a5d472325de2d38ae6c2cca8faa6146e3cf
ZefengQiu <z9qiu@uwaterloo.ca>
2016-03-14 15:54:41 -0400

    check packages

--- mining_main.py
+++ mining_main.py
@@ -3,8 +3,25 @@
 
 def main():
 
+    try:
+        import numpy
+        print "numpy is install"
+    except ImportError:
+        print "numpy is not installed"
 
-    return 
+
+    try:
+        import scipy
+        print "scipy is installed"
+    except ImportError:
+        print "scipy is not installed"
+
+
+    try:
+        import pandas
+        print "pandas is installed"
+    except ImportError:
+        print "pandas is not installed"
 
 
 if __name__ == "__main__":
